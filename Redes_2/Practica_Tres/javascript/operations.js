e382d134814ae7d37227eb7963778ffc6604d135
Andres Rl <arodartel1601@alumno.ipn.mx>
2019-04-11 01:48:35 -0500

    chaChat

---
+++ Redes_2/Practica_Tres/javascript/operations.js
@@ -0,0 +1,36 @@
+function fade(element, element2Unfade) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+			element.style.display = 'none';
+			unfade(element2Unfade)
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+	var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+function ingresar(){
+	document.getElementById('name').style.opacity = 0.1;
+	fade(document.getElementById('ingresar'), document.getElementById('name'));
+}
+function checkName() {
+	//Aquí checaremos que el nombre no este en uso, si todo cool mostramos la ventana de chat
+	document.getElementById('chat-area').style.opacity = 0.1;
+	fade(document.getElementById('welcome'), document.getElementById('chat-area'));
+}
